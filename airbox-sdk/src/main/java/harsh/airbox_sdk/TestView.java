775e80be25686dbf799cf04c3731b1d204eebb1b
Harsh Goyal <harshdeep@jungleworks.co>
2020-04-24 17:31:32 +0530

    change

---
+++ airbox-sdk/src/main/java/harsh/airbox_sdk/TestView.java
@@ -0,0 +1,16 @@
+package harsh.airbox_sdk;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class TestView extends LinearLayout {
+
+    public TestView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    private void init(Context context) {
+        inflate(context, R.layout.layout_text_view, this);
+    }
+}
